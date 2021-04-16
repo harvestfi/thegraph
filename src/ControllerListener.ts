d1233fb055230b62a60a8e39a1a1bcf5fa0c24cb
Jari <thelowsandhighs@gmail.com>
2021-04-16 14:07:17 +0200

    Controller listener maps new vaults. Added basic schema for strategies

--- src/ControllerListener.ts
+++ src/ControllerListener.ts
@@ -1,8 +1,41 @@
+import { log, BigInt } from '@graphprotocol/graph-ts'
+
+// subgraph templates
 import { VaultListener } from '../generated/templates'
 
-import { AddVaultAndStrategyCall } from "../generated/ControllerListener/Controller.ts"
+// contract imports
+import { AddVaultAndStrategyCall } from "../generated/ControllerListener/Controller"
+
+// schema imports
+import {
+  Vault,
+  Strategy
+} from "../generated/schema"
 
 
 export function handleAddVaultAndStrategy(call: AddVaultAndStrategyCall): void {
-  VaultListener.create(call.inputs._vault)
+  let vault_addr = call.inputs._vault
+  let strategy_addr = call.inputs._strategy
+
+  if (BigInt.fromUnsignedBytes(vault_addr) != BigInt.fromI32(0) &&
+      BigInt.fromUnsignedBytes(strategy_addr) != BigInt.fromI32(0)){
+    let vault = Vault.load(vault_addr.toHex())
+    if (vault == null) {
+      vault = new Vault(vault_addr.toHex())
+      vault.reg_block = call.block.number
+      vault.reg_timestamp = call.block.timestamp
+
+      let strategy = new Strategy(strategy_addr.toHex())
+      strategy.reg_block = call.block.number
+      strategy.reg_timestamp = call.block.timestamp
+      strategy.vault = vault.id
+
+      vault.curr_strat = strategy.id
+
+      vault.save()
+      strategy.save()
+
+      VaultListener.create(vault_addr)
+    }
+  }
 }
