0bf79d08b25b24a5a03275eab5b4abbb98be7fcf
Jari <thelowsandhighs@gmail.com>
2021-04-22 21:43:39 +0200

    Added DoHardWork entity and created mapping in ControllerListener, started with tutorial

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Currently work in progress to implement a subgraph for Harvest Finance
 
 
-### GraphQL Endpoint
+## GraphQL Endpoint
 
 The GraphQL endpoint as provided by [The Graph](https://thegraph.com) can be found here:
 https://api.thegraph.com/subgraphs/name/harvestfi/harvest-finance
@@ -15,17 +15,50 @@ Thegraph is build using the GraphQL api for querying information from its datast
 The main advantage of using GraphQL you only query the information you need. For the construction of such queries, refer to [learn GraphQL](https://graphql.org/learn/).
 
 As a small example let's create a query. First let's evaluate the database structure by using [Harvest The Graph Playground](https://thegraph.com/explorer/subgraph/harvestfi/harvest-finance).
+
+### Schema
 Currently the following schema is defined (which will be expanded on in the future, but shouldn't matter for the example):
 
     Vault
     Strategy
+    DoHardWork
     User
     Withdrawal
     Deposit
     Token
 
-These all represent entities which are contained within the data store. Each of these entities has fields which can be queried.
+These all represent entities which are contained within the data store. Each of these entities has fields which can be queried and can be inspected by clicking on the respective entities.
+
+### Fields
 For example a Vault entity contains, as of writing this, the following fields:
+
+    "address of the vault"
+    id: ID!
+
+    "block and timestamp on which the vault was registered at the controller"
+    reg_block: BigInt!
+    reg_timestamp: BigInt!
+
+    "current strategy"
+    curr_strategy: Strategy!
+
+    "all strategies"
+    strategies: [Strategy!]!
+
+    "token locked up in vault"
+    underlying: Token!
+
+    "returned share token"
+    f_token: Token!
+
+    "hard work done on vault"
+    do_hard_works: [DoHardWork!]!
+
+    withdrawals: [Withdrawal!]!
+    deposits: [Deposit!]!
+
+These are all fields that can be queried within a vault entity. As an example take `reg_block: BigInt!` which is named reg_block and contains a BigInt, the exclamation mark indicates that the field cannot return null and always exists. The following field `curr_strategy: Strategy!` returns a Strategy entity and has its respective fields that can be queried, these fields can be inspected by looking at the content of the Strategy entity. As you may observe some of the fields objects are enclosed by brackets, these are lists and therefore can return multiple values. As an example `strategies: [Strategy!]!` contain all strategies that were at some point attached to the vault. Although this field has both exclamation marks, it only means that this list exists and if it contains a Strategy it will be a Strategy, but it doesn't guarantee a Strategy will be contained within the list(in short - the list could be empty)
+
 
 
 
--- src/ControllerListener.ts
+++ src/ControllerListener.ts
@@ -4,7 +4,10 @@ import { log, BigInt, Address } from '@graphprotocol/graph-ts'
 import { VaultListener } from '../generated/templates'
 
 // contract imports
-import { AddVaultAndStrategyCall } from "../generated/ControllerListener/ControllerContract"
+import {
+  AddVaultAndStrategyCall,
+  SharePriceChangeLog as SharePriceChangeLogEvent
+} from "../generated/ControllerListener/ControllerContract"
 import { ERC20DetailedContract } from "../generated/ControllerListener/ERC20DetailedContract"
 import { VaultContract } from "../generated/ControllerListener/VaultContract"
 
@@ -12,8 +15,41 @@ import { VaultContract } from "../generated/ControllerListener/VaultContract"
 import {
   Vault,
   Strategy,
+  DoHardWork,
   Token
 } from "../generated/schema"
+
+export function handleSharePriceChangeLog(event: SharePriceChangeLogEvent): void{
+
+  let vault_addr = event.params.vault
+  let strategy_addr = event.params.strategy
+  let number = event.block.number
+  let timestamp = event.block.timestamp
+
+
+  // these only exist if the controllercontract has ben tracked since creation
+  let vault = Vault.load(vault_addr.toHex())
+  if (vault == null){
+    vault = createVaultAndStrategy(vault_addr, strategy_addr, number, timestamp)
+    log.warning('Vault didn\' exist yet, should only happen in testing, transaction hash:: {}', [
+        event.transaction.hash.toHex(),
+      ])
+  }
+
+  let strategy = Strategy.load(vault.curr_strategy)
+  let old_share_price = event.params.oldSharePrice
+  let new_share_price = event.params.newSharePrice
+
+  let do_hard_work = new DoHardWork(event.transaction.hash.toHex())
+  do_hard_work.block = number
+  do_hard_work.timestamp = timestamp
+  do_hard_work.vault = vault.id
+  do_hard_work.strategy = strategy.id
+  do_hard_work.old_share_price = old_share_price
+  do_hard_work.new_share_price = new_share_price
+  do_hard_work.save()
+
+}
 
 
 export function handleAddVaultAndStrategy(call: AddVaultAndStrategyCall): void {
@@ -25,33 +61,14 @@ export function handleAddVaultAndStrategy(call: AddVaultAndStrategyCall): void {
   if (BigInt.fromUnsignedBytes(vault_addr) != BigInt.fromI32(0) &&
       BigInt.fromUnsignedBytes(strategy_addr) != BigInt.fromI32(0)){
 
-    let vault_contract = VaultContract.bind(vault_addr)
     let vault = Vault.load(vault_addr.toHex())
     if (vault == null) {
 
-      let underlying_addr = vault_contract.underlying()
+      let number = call.block.number
+      let timestamp = call.block.timestamp
 
-      let underlying_token = loadOrCreateERC20DetailedToken(underlying_addr)
-      let f_token = loadOrCreateERC20DetailedToken(vault_addr)
+      vault = createVaultAndStrategy(vault_addr, strategy_addr, number, timestamp)
 
-
-      vault = new Vault(vault_addr.toHex())
-      vault.reg_block = call.block.number
-      vault.reg_timestamp = call.block.timestamp
-      vault.underlying = underlying_token.id
-      vault.f_token = f_token.id
-
-      let strategy = new Strategy(strategy_addr.toHex())
-      strategy.reg_block = call.block.number
-      strategy.reg_timestamp = call.block.timestamp
-      strategy.vault = vault.id
-
-      vault.curr_strategy = strategy.id
-
-      vault.save()
-      strategy.save()
-
-      VaultListener.create(vault_addr)
     }
   } else {
     // we log this incase calls do end up being called when transaction is reverted
@@ -61,6 +78,45 @@ export function handleAddVaultAndStrategy(call: AddVaultAndStrategyCall): void {
       ])
   }
 }
+
+
+function createVaultAndStrategy(
+  vault_addr: Address,
+  strategy_addr: Address,
+  number: BigInt,
+  timestamp: BigInt): Vault
+{
+
+  let vault_contract = VaultContract.bind(vault_addr)
+  let underlying_addr = vault_contract.underlying()
+
+  let underlying_token = loadOrCreateERC20DetailedToken(underlying_addr)
+  let f_token = loadOrCreateERC20DetailedToken(vault_addr)
+
+  let vault = new Vault(vault_addr.toHex())
+  vault.reg_block = number
+  vault.reg_timestamp = timestamp
+  vault.underlying = underlying_token.id
+  vault.f_token = f_token.id
+
+  // strategy cannot exist yet because it is unique to a vault and the only way
+  // for the enitity to be created is by getting here or the vault was already
+  // being listened to, which means the vault already exist so we wouldnt be here
+  let strategy = new Strategy(strategy_addr.toHex())
+  strategy.reg_block = number
+  strategy.reg_timestamp = timestamp
+  strategy.vault = vault.id
+
+  vault.curr_strategy = strategy.id
+
+  vault.save()
+  strategy.save()
+
+  VaultListener.create(vault_addr)
+  return vault
+}
+
+
 
 function loadOrCreateERC20DetailedToken(token_address: Address): Token{
   let token_contract = ERC20DetailedContract.bind(token_address)
