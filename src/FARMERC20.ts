e4da2948b1c69b02e86671231741399a5a775d59
Drondin <drondin596@gmail.com>
2021-05-31 10:42:39 +0200

    Use events

--- src/FARMERC20.ts
+++ src/FARMERC20.ts
@@ -1,14 +1,14 @@
 import { Address, log } from '@graphprotocol/graph-ts'
 
-import {  TransferCall  } from '../generated/FARMERC20/ERC20DetailedContract'
+import {  Transfer  } from '../generated/FARMERC20/ERC20DetailedContract'
 import { loadOrCreateNoMintPool } from "./utils/NoMintPool"
 
 
-export function handleTransfer(call: TransferCall): void {
-    let origin = call.from
-    let destination = call.inputs.recipient
+export function handleTransfer(event: Transfer): void {
+    let origin = event.params.from
+    let destination = event.params.to
 
     if (origin.toString() == "0xf00dd244228f51547f0563e60bca65a30fbf5f7f"){
-        loadOrCreateNoMintPool(destination, call.block, call.transaction)
+        loadOrCreateNoMintPool(destination, event.block, event.transaction)
     }
 }
