4111a1b764ca76220254844977918de3993c9a8f
Adam Becker <ad@mbecker.cc>
2021-06-25 00:56:42 -0700

    update test

--- test/hardworks.test.js
+++ test/hardworks.test.js
@@ -1,15 +1,45 @@
 const axios = require('axios')
-const { vaultIds } = require('./shared')
+const { request } = require('graphql-request')
+const vaults = require('./vaults.json').data
 const ETHPARSER_URL = 'https://ethparser-api.herokuapp.com/hardwork/pages'
+const GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/harvestfi/harvest-finance'
 
-it ('works properly', async () => {
-  const resp = await axios.get(ETHPARSER_URL, {
-    params: {
-      pageSize: 9999999,
-      page: 0,
-      vault: vaultIds[1]
+const GRAPHQL_QUERY = `
+{
+  vault(id: "$vaultId") {
+    id
+
+    doHardWorks {
+      id
     }
-  })
+  }
+}
+`
 
-  console.log(resp.data.data.data)
-})
+async function main() {
+  for (let vault of vaults.slice(0, 25)) {
+    console.log(`testing vault ${vault.contract.address}`)
+
+    const ethparserResponse = await axios.get(ETHPARSER_URL, {
+      params: {
+        pageSize: 9999999,
+        page: 0,
+        vault: vault.contract.address
+      }
+    })
+
+    let ethparserData = ethparserResponse.data.data.data
+    let ethparserCount = ethparserData ? ethparserData.length : 0
+
+    let graphQuery = GRAPHQL_QUERY.replace('$vaultId', vault.contract.address)
+    let graphResponse = await request(GRAPH_URL, graphQuery)
+    let graphData = graphResponse.vault ? graphResponse.vault.doHardWorks : []
+    let graphCount = graphData.length
+
+    console.log(`DoHardWorks on ethparser: ${ethparserCount}`)
+    console.log(`DoHardWorks on thegraph: ${graphCount}`)
+    console.log('')
+  }
+}
+
+main()
