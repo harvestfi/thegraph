a1b8dc352b53d7b9f5c71b8ee0b3b58326051bf4
bogdoslavik <bogdoslavik@gmail.com>
2021-08-26 16:16:08 +0500

    ContractRegistry (not deployed & not tested)

---
+++ src/ContractRegistryListener.ts
@@ -0,0 +1,65 @@
+import { log, BigInt, Address, ethereum } from '@graphprotocol/graph-ts'
+
+// contract imports
+import {
+  AddressesAdded,
+  AddressesRemoved,
+  PoolsAdded,
+  PoolsRemoved,
+  VaultsAdded,
+  VaultsRemoved
+
+} from "../generated/templates/ContractRegistryListener/ContractRegistry"
+
+
+// schema imports
+import {
+  ProfitLog,
+  ContractRegistryAddress,
+} from "../generated/schema"
+
+import { store } from '@graphprotocol/graph-ts'
+
+function addAddresses(addresses: Address[], contractType: string): void {
+  for (let i=0; i<addresses.length; i++) {
+    let address = addresses[i]
+    let contractRegistryAddress = new ContractRegistryAddress(address.toString())
+    contractRegistryAddress.address = address;
+    contractRegistryAddress.contractType = contractType;
+    contractRegistryAddress.save()
+  }
+}
+function removeAddresses(addresses: Address[]): void {
+  for (let i=0; i<addresses.length; i++) {
+    let address = addresses[i]
+    store.remove('ContractRegistryAddress', address.toString())
+  }
+}
+
+export function handleAddressesAdded(event: AddressesAdded): void {
+  let addresses = event.params.addresses
+  addAddresses(addresses, 'Unknown')
+}
+
+export function handleAddressesRemoved(event: AddressesRemoved): void {
+  let addresses = event.params.addresses
+  removeAddresses(addresses)
+}
+export function handlePoolsAdded(event: PoolsAdded): void {
+  let addresses = event.params.addresses
+  addAddresses(addresses, 'Pool')
+}
+
+export function handlePoolsRemoved(event: PoolsRemoved): void {
+  let addresses = event.params.addresses
+  removeAddresses(addresses)
+}
+export function handleVaultsAdded(event: VaultsAdded): void {
+  let addresses = event.params.addresses
+  addAddresses(addresses, 'Vault')
+}
+
+export function handleVaultsRemoved(event: VaultsRemoved): void {
+  let addresses = event.params.addresses
+  removeAddresses(addresses)
+}
